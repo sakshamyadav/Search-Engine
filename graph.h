d3678e29439dc60191480d53fc45c77473bf4730
houdeanie <30434131+houdeanie@users.noreply.github.com>
2017-10-09 14:54:13 +1100

    Create graph.h

---
+++ graph.h
@@ -0,0 +1,17 @@
+// Graph.h function protypes and graph adt interface
+// Created 9/10/2017
+// Modified: 9/10/2017
+
+// COMP2521 Assignment 2
+// Group: Ctrl+Alt+Elite
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <stdio.h>
+
+// graph representation is hidden
+typedef struct GraphRep *Graph;
+
+// graph operations
+Graph newGraph();
