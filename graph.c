d20e0b0c55805f845e64ee583f750a225f4eb1a9
houdeanie <30434131+houdeanie@users.noreply.github.com>
2017-10-09 14:48:19 +1100

    Update graph.c

--- graph.c
+++ graph.c
@@ -3,3 +3,12 @@
 // last modified: 9/10/2017
 
 // COMP2521 Assignment 2
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "Graph.h"
+
+typedef struct Graph {
+
