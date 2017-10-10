3a0aafc72f89cdd2c3df8e671b0364f5855a8fec
houdeanie <30434131+houdeanie@users.noreply.github.com>
2017-10-10 12:59:04 +1100

    Create pagerank.c

---
+++ pagerank.c
@@ -0,0 +1,21 @@
+/*
+You need to write a program in the file pagerank.c that reads data from a given collection of pages in the file collection.txt and 
+builds a graph structure using Adjacency Matrix or List Representation. 
+Using the algorithm described below, calculate Weighted PageRank for every url in the file collection.txt. 
+In this file, urls are separated by one or more spaces or/and new line character. 
+Add suffix .txt to a url to obtain file name of the corresponding "web page". 
+For example, file url24.txt contains the required information for url24.
+*/
+// Created 10/10/2017
+// last modified: 10/10/2017
+
+// COMP2521 Assignment 2
+// Group: Ctrl+Alt+Elit
+
+#include <sdtio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main() {
+  return 0;
+}
