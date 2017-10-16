0472a04c7455908c059660c77b78831b526d7811
Saksham Yadav <sakshamyadav11@gmail.com>
2017-10-17 03:17:06 +1100

    Add files via upload

--- inverted.c
+++ inverted.c
@@ -48,12 +48,12 @@ int main(void) {
     int read;
     char word[MAX_LENGTH];
     char **wordArray = NULL;
-	char urlName[100], fileType[100];
+    char urlName[100], fileType[100];
 
-	//begin loop to store each word in every file into wordArray
+    //begin loop to store each word in every file into wordArray
     for(i=0; i<numURLs; i++){
-    	strcpy(urlName, urlArray[i]);
-    	strcpy(fileType, ".txt");
+        strcpy(urlName, urlArray[i]);
+        strcpy(fileType, ".txt");
         char *currentURL = strcat(urlName, fileType);
         url = fopen(currentURL, "r");
         read = 0; // reset read for each file opened
@@ -64,9 +64,9 @@ int main(void) {
             }
             //if 'word' is in the body of the URL (section-2), read it in
             if (read == 1) { // read only when 1, i.e. after 1st Section-2 is seen
-            	//apply normalise function to word to remove punctuation marks and convert to lowercase
-            	normalise(word);
-            	//check to make sure that the word doesn't already exist in array
+                //apply normalise function to word to remove punctuation marks and convert to lowercase
+                normalise(word);
+                //check to make sure that the word doesn't already exist in array
                 if (duplicates(wordArray, word, nWords) != 1) { // not duplicated
                     wordArray = realloc(wordArray, (nWords+1)*sizeof(char*));
                     wordArray[nWords] = malloc(strlen(word)+1);
@@ -77,7 +77,7 @@ int main(void) {
                 read++;
             }
         }
-   		//close file
+        //close file
         fclose(url);
     }
 
@@ -110,81 +110,81 @@ int duplicates(char **str, char *in, int size) {
 
 //normalise function - convert all characters to lowercase and remove punctuation marks
 void normalise(char *ch){
-	int i;
-	char *tempChar, *tempStr;
+    int i;
+    char *tempChar, *tempStr;
 
-	//convert each word to lowercase
-	for(i=0; i<strlen(ch); i++){
-		ch[i] = tolower(ch[i]);
-	}
-	
-	//remove ".", ",", "?", ";" characters
-	tempStr = tempChar = ch;
-	while(*tempStr){
-		*tempChar = *tempStr++;
-       	if((*tempChar != '.' && *tempChar != ','  && *tempChar != ';'  && *tempChar != '?' && *tempChar != ' ' )){
-       		tempChar++;
-       	}
-	}
-	*tempChar = '\0'; 
+    //convert each word to lowercase
+    for(i=0; i<strlen(ch); i++){
+        ch[i] = tolower(ch[i]);
+    }
+    
+    //remove ".", ",", "?", ";" characters
+    tempStr = tempChar = ch;
+    while(*tempStr){
+        *tempChar = *tempStr++;
+        if((*tempChar != '.' && *tempChar != ','  && *tempChar != ';'  && *tempChar != '?' && *tempChar != ' ' )){
+            tempChar++;
+        }
+    }
+    *tempChar = '\0'; 
 }
 
 //sort wordArray and urlArray in alphabetical order using a bubble sort
 void sortAlpha(char **str, int length){
-	int i, j; 
-	char *temp;
+    int i, j; 
+    char *temp;
 
-	//sort wordArray in alphabetical order
-	for(i=0; i<length; i++){
-		for(j=i+1; j<length; j++){
-			if(strcmp(str[i], str[j])>0){
-				temp = str[i];
-				str[i] = str[j];
-				str[j] = temp;
-			}
-		}
-	}
+    //sort wordArray in alphabetical order
+    for(i=0; i<length; i++){
+        for(j=i+1; j<length; j++){
+            if(strcmp(str[i], str[j])>0){
+                temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
 }
 
 //write words and URLs to file 
 void writetoFile(char **str, char **url, int length, int urlLength){
-	int i, j; 
-	FILE *fp; 
-	//open file for writing
-	FILE *write = fopen("invertedIndex.txt", "w");
+    int i, j; 
+    FILE *fp; 
+    //open file for writing
+    FILE *write = fopen("invertedIndex.txt", "w");
 
-	//begin loop for each word 
-	for(i=0; i<length; i++){
-		//write the current word to file
-		fprintf(write, "%s ", str[i]);
-		//begin loop for each URL
-		for(j=0; j<urlLength; j++){
-			char urlName[100], fileType[100], temp[3000];
-        	strcpy(urlName, url[j]);
-        	strcpy(fileType, ".txt");
-        	char *current = strcat(urlName,fileType);
+    //begin loop for each word 
+    for(i=0; i<length; i++){
+        //write the current word to file
+        fprintf(write, "%s ", str[i]);
+        //begin loop for each URL
+        for(j=0; j<urlLength; j++){
+            char urlName[100], fileType[100], temp[3000];
+            strcpy(urlName, url[j]);
+            strcpy(fileType, ".txt");
+            char *current = strcat(urlName,fileType);
 
-        	//open URL for reading
-        	fp = fopen(current, "r");
+            //open URL for reading
+            fp = fopen(current, "r");
 
-        	while(fscanf(fp, "%s", temp)!=EOF){
-        		//apply normalise function to word for comparison 
-        		normalise(temp);
-        		//if the word being read is equal to the word currently being compared against, then the word exists in this URL - so write it to file
-            	if(strcmp(temp, str[i])==0){
-            		//write word to file 
-                	fprintf(write, "%s ", url[j]);
-                	break;
-            	}
-        	}
-        	//close URL file
-        	fclose(fp);
-		}
-		//write a newline at the end of each loop until last word is reached 
-		if(i != length-1){
-			fprintf(write, "\n");
-		}
-	}
-	//close invertedIndex file (the one being written to)
-	fclose(write);
+            while(fscanf(fp, "%s", temp)!=EOF){
+                //apply normalise function to word for comparison 
+                normalise(temp);
+                //if the word being read is equal to the word currently being compared against, then the word exists in this URL - so write it to file
+                if(strcmp(temp, str[i])==0){
+                    //write word to file 
+                    fprintf(write, "%s ", url[j]);
+                    break;
+                }
+            }
+            //close URL file
+            fclose(fp);
+        }
+        //write a newline at the end of each loop until last word is reached 
+        if(i != length-1){
+            fprintf(write, "\n");
+        }
+    }
+    //close invertedIndex file (the one being written to)
+    fclose(write);
 }
