11dc8db1b389fbdfabff76acc82129135abbb839
Yathu Arulnanthy <yathu@outlook.com>
2017-10-20 19:02:52 +1100

    Updated scaleFootrule.c --- works with multiple fi

--- scaledFootrule.c
+++ scaledFootrule.c
@@ -10,58 +10,113 @@ int optimumPos[100];
 void sort(int *pr, int length);
 void positions(int *pos, int *unranked, int *ranked, int length);
 void swap(int *pos, int i, int j);
-void permute(int *array,int i,int length, int *pos1, int pos1Len, int *pos2, int pos2Len);
+void permute(int *array,int i,int length, int pos[100][100], int *numUrls, int argc);
 int main(int argc, char **argv) {
 
 	if(argc < 2) {
 		fprintf(stderr, "Usage: %s rankA.txt  rankD.txt", argv[0]);
 		exit(1);
 	}
+	int i;
 	//opening the first file (e.g. rankA.txt)
-	FILE *fp1;
-	fp1 = fopen(argv[1], "r");
+	//FILE *fp;
+	int mainMatrix[1000][1000];
+	int mainMatrixS[1000][1000];
+	int numUrls[1000] = {0};
+	char string[3000];
+
+	for(i = 0; i < argc-1; i++) {
+		FILE *fp;
+		fp = fopen(argv[i+1], "r");
+		int j = 0;
+		while(fscanf(fp, "%s", string) != EOF) {
+			char *token = strtok(string, "url");
+			mainMatrix[i][j] = mainMatrixS[i][j] = atoi(token);
+			j++;
+			numUrls[i]++;
+		}
+		fclose(fp);
+	}
+
+
+	printf("Matrix[0][0]: %d\n", mainMatrix[0][0]);
+	printf("Matrix[0][1]: %d\n", mainMatrix[0][1]);
+	printf("Matrix[0][2]: %d\n", mainMatrix[0][2]);
+	printf("Matrix[0][3]: %d\n", mainMatrix[0][3]);
+	printf("Matrix[0][3]: %d\n", mainMatrix[0][4]);
+	printf("Matrix[1][0]: %d\n", mainMatrix[1][0]);
+	printf("Matrix[1][1]: %d\n", mainMatrix[1][1]);
+	printf("Matrix[1][2]: %d\n", mainMatrix[1][2]);
+	printf("Matrix[1][3]: %d\n", mainMatrix[1][3]);
+	printf("numUrls[0]: %d\n", numUrls[0]);
+	printf("numUrls[1]: %d\n", numUrls[1]);
+
+	//char ***urlarray = (char***) malloc(numUrls*sizeof(char**));
+
+/*
+	for(i = 0; i < argc-1; i++) {
+		fp = fopen(argv[i+1], "r");
+		while (fscanf(fp, "%s", string) != EOF) {
+			urlarray = realloc(urlarray, (numUrls+1)*sizeof(char**));
+
+			urlarray[numUrls] = malloc(strlen(string)+1);
+			strcpy(urlarray[numUrls++], string);
+		}
+	}*/
 
 	//opening the second file (e.g. rankB.txt)
-	FILE *fp2;
-	fp2 = fopen(argv[2], "r");
 
 	//no. of items in the first file (rankA.txt)
-	int numItems1 = 0;
+	//int numItems1 = 0;
 	//no. of items in the second file (rankB.txt)
-	int numItems2 = 0;
+	//int numItems2 = 0;
 
-	char chararr[100];
+	//char chararr[100];
 
 
-	int rank1[100];
-	int rank2[100];
+	// int rank1[100];
+	// int rank2[100];
 
-	int rank1S[100];
-	int rank2S[100];
+	// int rank1S[100];
+	// int rank2S[100];
 
-	int pos1[100];
-	int pos2[100];
+	// int pos1[100];
+	// int pos2[100];
 
 	//rankx[] array stores the no suffix of the url 
 	//rankxS[] array stores the sorted no suffix of the url
-	while(fscanf(fp1, "%s", chararr) != EOF)	{
-	    rank1[numItems1] = rank1S[numItems1] = atoi(chararr+3);
-	    numItems1++;
+	// while(fscanf(fp1, "%s", chararr) != EOF)	{
+	// 	char *token = strtok(chararr, "url");
+	//     rank1[numItems1] = rank1S[numItems1] = atoi(token);
+	//     numItems1++;
+	// }
+
+	// while(fscanf(fp2, "%s", chararr) != EOF)	{
+	// 	char *token = strtok(chararr, "url");
+	// 	rank2[numItems2] = rank2S[numItems2]= atoi(token);
+	// 	numItems2++;
+	// }
+
+	int numUrlsMax = 0;
+
+	for(i = 0; i < argc-1; i++) {
+		if(numUrls[i]>numUrlsMax) numUrlsMax = numUrls[i];
 	}
-	printf(" numitems1: %d\n", numItems1);
+	printf("numUrls: %d\n", numUrlsMax);
 
-	while(fscanf(fp2, "%s", chararr) != EOF)	{
-		rank2[numItems2] = rank2S[numItems2]= atoi(chararr+3);
-		numItems2++;
+	// fclose(fp1);
+	// fclose(fp2);
+
+	//Sorting array
+	for(i = 0; i < argc-1; i++) {
+		sort(mainMatrixS[i], numUrls[i]);
 	}
 
-	int numItems = (numItems1 > numItems2) ? numItems1:numItems2;
-
-	fclose(fp1);
-	fclose(fp2);
-
-	sort(rank1S, numItems1);
-	sort(rank2S, numItems2);
+	for(i = 0; i < numUrls[0]; i++) {
+		printf("Matrix Sorted: %d\n", mainMatrixS[0][i]);
+	}
+	// sort(rank1S, numItems1);
+	// sort(rank2S, numItems2);
 
 	//position function makes an array with the position of the urls
 	//e.g. 
@@ -74,27 +129,38 @@ int main(int argc, char **argv) {
 	pos1[4,2,1,3]
 	As url1 has 4th position, url2 has 2nd position, url3 has 1st position and url4 has 3rd position
 	*/ 
-	positions(pos1, rank1, rank1S, numItems1);
-	positions(pos2, rank2, rank2S, numItems2);
+	// positions(pos1, rank1, rank1S, numItems1);
+	// positions(pos2, rank2, rank2S, numItems2);
+	int pos[100][100];
+
+	for(i = 0; i < argc-1; i++) {
+		positions(pos[i], mainMatrix[i], mainMatrixS[i], numUrls[i]);
+	}
+
+	for(i = 0; i < numUrls[0]; i++) {
+		printf("Position: %d\n", pos[0][i]);
+	}
 
 	//orderedPosArray creates an array with ordered numbers upto the the max no. of items in the biggest array.
 	//e.g. rankA.txt has 5 items rankB.txt has 4 items, therefore orderedPosArray would contain [1,2,3,4,5]
 	//Purpose - to pass into permute function and change the order to find the smallest scale foot distance.
 	int orderedPosArray[100];
-	int i;
-	for(i = 0; i < numItems; i++) {
+	//int i;
+	for(i = 0; i < numUrlsMax; i++) {
 		orderedPosArray[i] = i+1;
-		printf("%d\n", orderedPosArray[i]);
+		printf("orderedPos%d\n", orderedPosArray[i]);
 	}
 
-	//double scaleFootrule = 100;
+	// //double scaleFootrule = 100;
 
-	printf("RankA: 1st: %d\nRankB: 1st: %d\nRank2S: 1st: %d\nPos: 1st: %d\n", rank1[0], rank2[0], rank2S[0], pos2[0]);
-	permute(orderedPosArray, 0, numItems, pos1, numItems1, pos2, numItems2);
-	printf("squarerile: %lf\n", scaleFootrule);
-	for(i = 0; i < numItems; i++) {
-		printf("%d\n", optimumPos[i]);
-	}
+	permute(orderedPosArray, 0, numUrlsMax, pos, numUrls, argc);
+	printf("%lf\n", scaleFootrule);
+	
+	// for(i = 0; i < numItems; i++) {
+	// 	char placeholder[20];
+	// 	sprintf(placeholder,"url%d",optimumPos[i]);
+	// 	printf("%s\n", placeholder);
+	// }
 
 	return 0;
 
@@ -118,14 +184,15 @@ void sort(int *pr, int length){
 
 }
 
-void positions(int *pos, int *unranked, int *ranked, int length) {
+void positions(int *pos, int *unsorted, int *sorted, int length) {
 	int posNum = 0;
 
 	int i, j;
 	for(i = 0; i < length; i++) {
 		for(j = 0; j < length; j++) {
-			if(ranked[i] == unranked[j]) {
+			if(sorted[i] == unsorted[j]) {
 				pos[posNum++] = j+1;
+				break;
 			}
 		}
 	}
@@ -138,45 +205,47 @@ void swap(int *pos, int i, int j) {
 }
 
 //This function needs correcting.
-void permute(int *array,int i,int length, int *pos1, int pos1Len, int *pos2, int pos2Len) { 
+void permute(int *array,int i,int length, int pos[100][100], int *numUrls, int argc) { 
 
 	double temp = 0;
-	int j, k;
+	int j, l;
+	//int k;
 
   	if (i == length){
 
-     	for(j = 0; j < pos1Len; j++) {
-     		printf("small cla:%lf\n", ((double)pos1[j]/(double)length));
-     		temp += fabs(((double)pos1[j]/(double)pos1Len) - ((double)array[j]/(double)length));
-     		printf("1********************%lf\n", temp);
-     	}
+  		for(l = 0; l < argc-1; l++) {
+  			for(j = 0; j < numUrls[l]; j++) {
+  				temp += fabs(((double)pos[l][j]/(double)numUrls[l]) - ((double)array[j]/(double)length));
+  			}
+  		}
 
-     	for(j = 0; j < pos2Len; j++) {
-     		temp += fabs(((double)pos2[j]/(double)pos2Len - (double)array[j]/(double)length));
-     		printf("2********************%lf\n", temp);
-     	}
-     	printf("3********************%lf\n", temp);
+  		
+
+     	// for(j = 0; j < pos1Len; j++) {
+     	// 	temp += fabs(((double)pos1[j]/(double)pos1Len) - ((double)array[j]/(double)length));
+     	// }
+
+     	// for(j = 0; j < pos2Len; j++) {
+     	// 	temp += fabs(((double)pos2[j]/(double)pos2Len - (double)array[j]/(double)length));
+     	// }
      	if(temp < scaleFootrule) {
      		scaleFootrule = temp;
-     		for(k = 0; k < length; k++) {
-     			if(pos1Len > pos2Len) {
-     				optimumPos[k] = pos1[k];
-     			} else {
-     				optimumPos[k] = pos2[k];
-     			}
+     		// for(k = 0; k < length; k++) {
+     		// 	if(pos1Len > pos2Len) {
+     		// 		optimumPos[k] = pos1[k];
+     		// 	} else {
+     		// 		optimumPos[k] = pos2[k];
+     		// 	}
      			
-     		}
+     		// }
 
      	}
-     	printf("4********************%lf\n", scaleFootrule);
     	return;
   	}
 	for (j = i; j < length; j++) { 
-		printf("i: %d\nj: %d\n", i, j);
  		swap(array,i,j);
-		permute(array,i+1, length, pos1, pos1Len, pos2, pos2Len);
+		permute(array,i+1, length, pos, numUrls, argc);
 		//swap(array,i,j);
-		printf("********************%lf\n", scaleFootrule);
 	}
 	return;
 }
